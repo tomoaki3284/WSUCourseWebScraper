6f9af6830a1869b1460eb5194845f5bdb6c98d7a
Tomoaki Mitsuhashi <tmitsuhashi9621@westfield.ma.edu>
2020-03-21 00:14:03 -0400

    Start working on Unit Test

---
+++ src/test/java/tomoaki/courseClasses/HoursTest.java
@@ -0,0 +1,17 @@
+package tomoaki.courseClasses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HoursTest {
+	
+	@Test
+	public void getHour() {
+		
+	}
+	
+	@Test
+	public void getMilitaryTime() {
+	}
+}
